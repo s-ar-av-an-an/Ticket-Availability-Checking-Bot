07f654fdf3c743cdfe4d1a4cfd5dd2dddef50cde
s-ar-av-an-an <120708248+s-ar-av-an-an@users.noreply.github.com>
2023-12-09 14:44:03 +0530

    Delete initialize.sh

--- initialize.sh
+++
@@ -1,9 +0,0 @@
-apt update
-apt install libxml2 libxslt -y
-apt install python -y
-apt install termux-api -y
-apt install termux-services -y
-apt install cronie -y
-apt install which -y
-pkg install libexpat
-pkg install openssl
